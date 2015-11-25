b5da700dbed73639f35e7141a7caa36844f6473b
Nathan S <nathan.starkweather@gmail.com>
2015-11-25 12:43:39 -0800

    mixing time working, doing some changes to make it more useful

---
+++ analysis/mixing_time_interface.py
@@ -0,0 +1,188 @@
+"""
+
+Created by: Nathan Starkweather
+Created on: 11/24/2015
+Created in: PyCharm Community Edition
+
+
+"""
+__author__ = 'Nathan Starkweather'
+
+import tkinter as tk
+import tkinter.ttk as ttk
+
+from hello.hello import HelloApp
+from hello import logger
+
+_logger = logger.BuiltinLogger(__name__)
+_info = _logger.info
+_debug = _logger.debug
+
+
+class SimpleMenu(ttk.OptionMenu):
+    def __init__(self, master, initvalue, options):
+        self.var = tk.StringVar(None, initvalue)
+        ttk.OptionMenu.__init__(self, master, self.var, None, *options)
+
+    def get(self):
+        return self.var.get()
+
+
+class ItemButton(ttk.Button):
+    def __init__(self, master, name, cmd, **kw):
+        self.tv = tk.StringVar(None, name)
+        super().__init__(master, textvariable=self.tv, command=cmd, **kw)
+
+    def grid(self, row, col, **kwargs):
+        super().grid(row=row, column=col, **kwargs)
+
+
+class StatefulItemButton(ItemButton):
+    def __init__(self, master, name, initial_cmd):
+        super().__init__(master, name, self._do_cmd)
+        self._cmd = initial_cmd
+
+    def _do_cmd(self, *args, **kwargs):
+        self._cmd(*args, **kwargs)
+
+    def set_cmd(self, cmd):
+        self._cmd = cmd
+
+
+class SimpleEntryButton():
+    def __init__(self, root, frame_text, button_text, button_cmd):
+        self.frame = ttk.Frame(root)
+        self.label = ttk.Label(self.frame, text=frame_text)
+        self.entry_tv = tk.StringVar()
+        self.entry = ttk.Entry(self.frame, textvariable=self.entry_tv)
+        self.button = StatefulItemButton(self.frame, button_text, button_cmd)
+
+    def grid(self, row, col):
+        self.frame.grid(row=row, column=col, rowspan=2, sticky=tk.N)
+        self.entry.grid(row=1, column=1, columnspan=1)
+        self.button.grid(1, 2)
+        self.label.grid(row=0, column=1, sticky=tk.W)
+
+    def grid_forget(self):
+        for w in (self.frame, self.entry, self.button, self.label):
+            w.grid_forget()
+
+    def get_entry_text(self):
+        return self.entry_tv.get()
+
+
+class SimpleListbox():
+    def __init__(self, root, label_text):
+        self.frame = ttk.Frame(root)
+        self.label = ttk.Label(root, text=label_text)
+        self.listbox = tk.Listbox(root)
+
+    def grid(self, row, col):
+        _debug("Gridding")
+        self.frame.grid(row=row, column=col)
+        self.label.grid(row=0, column=0)
+        self.listbox.grid(row=1, column=0)
+
+    def grid_forget(self):
+        for w in (self.frame, self.label, self.listbox):
+            w.grid_forget()
+
+    def insert(self, index, items):
+        self.listbox.insert(index, *items)
+
+    def delete(self, first, last):
+        self.listbox.delete(first, last)
+
+    def clear(self):
+        self.delete(0, tk.END)
+
+
+class IPAddyFrameView():
+    def __init__(self, root):
+        self.frame = ttk.LabelFrame(root, text="Open by IP Address")
+        self.batch_listbox = SimpleListbox(self.frame, "Batch List:")
+        self.gb_frame = SimpleEntryButton(self.frame, "Enter IP Address:", "get Batches!", self._do_gb_cb)
+
+    def _do_gb_cb(self):
+        self.getbatches_btn_cb()
+
+    def getbatches_btn_cb(self):
+        pass  # hook
+
+    def insert_batches(self, names):
+        _debug("Inserting %s names", len(names) if hasattr(names, "__len__") else "??")
+        self.batch_listbox.insert(tk.END, names)
+
+    def clear_batches(self):
+        _debug("Clearing all batches")
+        self.batch_listbox.clear()
+
+    def grid(self):
+        _debug("Gridding %s" % self.__class__.__name__)
+        self.frame.grid()
+        self.batch_listbox.grid(0, 0)
+        self.gb_frame.grid(0, 1)
+
+    def grid_forget(self):
+        self.gb_frame.grid_forget()
+        self.batch_listbox.grid_forget()
+        self.frame.grid_forget()
+
+    def get_ipv4(self):
+        return self.gb_frame.get_entry_text()
+
+
+class IPAddyFrameModel():
+    def __init__(self):
+        self._batch_list = None
+
+    def get_batches(self, ipv4):
+        _debug("Getting batches from %s", ipv4)
+        app = HelloApp(ipv4)
+        batches = app.getBatches()
+        self._batch_list = batches
+        return batches
+
+
+class IPAddyFrameWidget():
+    def __init__(self, root, autogrid=True):
+
+        # create model/view
+        self.view = IPAddyFrameView(root)
+        self.model = IPAddyFrameModel()
+
+        # configure view callbacks
+        self.view.getbatches_btn_cb = self.get_batches
+
+        if autogrid:
+            self.view.grid()
+
+    def get_batches(self):
+        _debug("callback called")
+        ipv4 = self.view.get_ipv4()
+        batches = self.model.get_batches(ipv4)
+        self.view.clear_batches()
+        self.view.insert_batches(b.name for b in batches.ids_to_batches.values())
+
+    def grid(self):
+        self.view.grid()
+
+    def grid_forget(self):
+        self.view.grid_forget()
+
+
+class MixingTimeInterface():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.frame = IPAddyFrameWidget(self.root)
+
+        self.root.grid()
+
+    def mainloop(self):
+        _debug("Beginning mainloop")
+        self.root.mainloop()
+
+if __name__ == '__main__':
+    m = MixingTimeInterface()
+    m.frame.view.gb_frame.entry_tv.set("192.168.1.7")
+    m.mainloop()
--- analysis/mixingtime.py
+++ analysis/mixingtime.py
@@ -11,9 +11,12 @@ __author__ = 'Nathan Starkweather'
 
 from officelib.xllib import xlcom, xladdress
 from officelib import const
-import tempfile
 from hello.hello import HelloApp
-from hello.logger import BuiltinLogger
+from hello.logger import BuiltinLogger, logging
+
+import tempfile
+import os
+import math
 
 ###################
 # Utility Functions
@@ -27,17 +30,35 @@ def _extract_cell_data(cells, top_left, bottom_right):
 def _ave(o):
     return sum(o) / len(o)
 
+def _get_filename(path):
+    return os.path.splitext(os.path.basename(path))[0]
+
 ############
 # Exceptions
 ############
 
 
-class MixingTimeAnalysisException(Exception):
+class MixingTimeAnalysisError(Exception):
     pass
 
 #########
 # Classes
 #########
+
+
+class CompiledMixingSaveAsInfo():
+    def __init__(self, path):
+        self.path = os.path.normpath(path)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    def save_mt(self, mt):
+        filename = os.path.join(self.path, mt.name) + ".xlsx"
+        mt.wb.SaveAs(Filename=filename)
+
+    def save_compiled(self, ct):
+        filename = os.path.join(self.path, ct.name) + ".xlsx"
+        ct.wb.SaveAs(filename)
 
 
 class CompiledMixingTime():
@@ -48,34 +69,115 @@ class CompiledMixingTime():
      Intended to be the primary entry point. Uses a BuiltinLogger
      passed on to all child tests created through its interface.
     """
-    def __init__(self, xl=None, wb=None, logger=None):
+    def __init__(self, name=None, xl=None, wb=None, logger=None, saveas_info=None):
+
+        if xl or wb:
+            if xl:
+                wb = xl.Workbooks.Add()
+            elif wb:
+                xl = wb.Application
+        else:
+            xl, wb = xlcom.xlBook2()
+
         self.xl = xl
         self.wb = wb
-        self.logger = logger or BuiltinLogger(self.__class__.__name__)
+        self.ws = wb.Worksheets(1)
+        self.init_ws()
+        self.name = name or "Compiled Data"
+        self.logger = logger or BuiltinLogger(self.__class__.__name__, logging.INFO)
         self.tests = []
+        self.compiled_tests = 0
+
+        self.saveas_info = saveas_info or CompiledMixingSaveAsInfo("C:/.replcache/mixing test/")
+
+    def init_ws(self):
+        self.ws.Cells(2, 21).Value = "Name"
+        self.ws.Cells(2, 22).Value = "T95"
+        self.ws.Cells(2, 23).Value = "stdev"
 
     def analyze_all(self):
-        for mt in self.tests:
-            mt.run_analysis()
-            self.add_to_compiled(mt)
+        self.tests.sort(key=lambda mt: mt.name[-3:])
+        with xlcom.HiddenXl(self.xl, True):
+            self.logger.info("Running analysis on %d tests.", len(self.tests))
+            for mt in self.tests:
+                mt.run_analysis()
+                self.add_to_compiled(mt)
+                self.save_test(mt)
+        self.saveas_info.save_compiled(self)
+
+    def save_test(self, mt):
+        self.saveas_info.save_mt(mt)
 
     def add_csv(self, csv):
         mt = self._create_test_from_csv(csv)
         self.add_test(mt)
         return mt
 
+    def add_frombatchname(self, ipv4, batch_name):
+        mt = MixingTimeTest.from_batchname(ipv4, batch_name, None, self.logger)
+        self.add_test(mt)
+        return mt
+
+    def add_batchnames(self, ipv4, batch_names):
+        with xlcom.HiddenXl(self.xl, True):
+            app = HelloApp(ipv4)
+            app.login()
+            batches = app.getBatches()
+            for name in batch_names:
+                id = batches.getbatchid(name)
+                report = app.getdatareport_bybatchid(id)
+                mt = MixingTimeTest.from_bytes(report, name, self.logger)
+                self.add_test(mt)
+
     def add_test(self, test):
         self.tests.append(test)
 
     def _create_test_from_csv(self, file):
-        return MixingTimeTest.from_csv(file, self.logger)
+        return MixingTimeTest.from_csv(file, None, self.logger)
 
     def from_csv_list(self, files):
         for file in files:
             self.add_csv(file)
 
+    def _add_chart_to_compiled(self, mixing_test):
+        self.logger.debug("Adding test to compiled data worksheet")
+        # fixme: move to config area(?)
+        charts_per_row = 3
+        chart_left_start = 30
+        chart_top_start = 30
+        chart_left_padding = 5
+        chart_top_padding = 5
+        mt_ws = mixing_test.ws
+        mt_chart_ob = mt_ws.ChartObjects(1)
+        chart = mt_chart_ob.Chart
+        chart.ChartArea.Copy()
+        self.ws.Paste()
+        chart_objs = self.ws.ChartObjects()
+        chart_count = chart_objs.Count
+        chart_obj = chart_objs(chart_count)
+        chart_width = mt_chart_ob.Width
+        chart_height = mt_chart_ob.Height
+        chart_count -= 1
+        chart_left = (chart_count % charts_per_row) * chart_width + \
+                     (chart_count % charts_per_row) * chart_left_padding + chart_left_start
+        chart_top = (chart_count // charts_per_row) * chart_height + \
+                    (chart_count // charts_per_row) * chart_top_padding + chart_top_start
+        chart_obj.Height = chart_height
+        chart_obj.Width = chart_width
+        chart_obj.Left = chart_left
+        chart_obj.Top = chart_top
+        xlcom.FormatChart(chart_obj.Chart, None, mixing_test.name)
+
     def add_to_compiled(self, mixing_test):
-        raise NotImplementedError
+
+        self._add_chart_to_compiled(mixing_test)
+
+        cells = self.ws.Cells
+        row = self.compiled_tests + 3
+        cells(row, 21).Value = mixing_test.name
+        cells(row, 22).Value = mixing_test.t95
+
+        self.compiled_tests += 1
 
 
 class MixingTimeTest():
@@ -86,14 +188,22 @@ class MixingTimeTest():
     Intended to be instantized by above CompiledMixingTime. Can
     be used directly for individual tests or customization.
     """
-    def __init__(self, xl=None, wb=None, logger=None, conductivity_var="phARaw"):
+    def __init__(self, name=None, xl=None, wb=None, logger=None, conductivity_var="phARaw"):
+        self.logger = logger or BuiltinLogger(self.__class__.__name__)
         self.xl = xl
         self.wb = wb
+        self.ws = wb.Worksheets(1)
+        self.name = name
+
+        self.logger.debug("New Test with name %s", name if name else "<no name>")
+
+        if name:
+            self.ws.Name = name
+
         self.cv = conductivity_var
-        self.logger = logger or BuiltinLogger(self.__class__.__name__)
 
     @classmethod
-    def from_csv(cls, csv, logger=None):
+    def from_csv(cls, csv, name=None, logger=None):
         """
         @param csv: filename of csv file
         @param logger: Logger
@@ -102,10 +212,19 @@ class MixingTimeTest():
         Enter function at IP00043 Rev A 7.4.2
         """
         xl, wb = xlcom.xlBook2(csv)
-        return cls(xl, wb, logger)
+        if name is None:
+            name = _get_filename(csv)
+        return cls(name, xl, wb, logger)
 
     @classmethod
-    def from_batchname(cls, ipv4, batch_name, logger=None):
+    def from_bytes(cls, report, name=None, logger=None):
+        with tempfile.NamedTemporaryFile("wb", suffix=".csv", delete=False) as t:
+            t.write(report)
+        self = cls.from_csv(t.name, name, logger)
+        return self
+
+    @classmethod
+    def from_batchname(cls, ipv4, batch_name, name=None, logger=None):
         """
         @param ipv4: IP address of bioreactor
         @param batch_name: batch name of test batch
@@ -116,17 +235,21 @@ class MixingTimeTest():
         app = HelloApp(ipv4)
         app.login()
         r = app.getdatareport_bybatchname(batch_name)
-        t = tempfile.NamedTemporaryFile("wb", suffix=".csv")
-        t.write(r)
-        return cls.from_csv(t.name, logger)
+
+        if name is None:
+            name = batch_name
+
+        return cls.from_bytes(r, name, logger)
 
     def _find_logger_ts_1k(self, cells):
         self.logger.debug("Searching for logger column: \"%s\"", self.cv)
         logger_col = cells.Find(What="LoggerMaxLogInterval(ms)",
                                 After=cells(1, 1), SearchOrder=const.xlByRows)
         logger_col_values = cells.Columns(logger_col.Column + 1)
+        self.logger.debug("Searching for first instance of \"1000\"")
         first_1k = logger_col_values.Find(What="1000", SearchOrder=const.xlByColumns)
         logger_timestamp = cells(first_1k.Row, logger_col.Column).Value
+        self.logger.debug("Found logger timestamp: %s", logger_timestamp)
         return logger_timestamp
 
     def _add_cond_et_col(self, cells, logger_timestamp):
@@ -134,30 +257,36 @@ class MixingTimeTest():
         cc = cells.Find(What=self.cv, After=cells(1, 1), SearchOrder=const.xlByRows)
         cc_col = cc.Column
         cc_end_row = cc.End(const.xlDown).Row
+        self.logger.debug("Extracting conductivity data")
         cond_data = _extract_cell_data(cells, cc, cells(cc_end_row, cc_col + 1))
         ts_data = cond_data[0][1:]
         for row, data in enumerate(ts_data, 2):
             if data > logger_timestamp:
                 break
         else:
-            raise MixingTimeAnalysisException("Failed to find conductivity start timestamp.")
+            raise MixingTimeAnalysisError("Failed to find conductivity start timestamp.")
+        self.logger.debug("Found data. Generating Elapsed Time column")
         et_formulas = self._generate_et_formulas(row, cc_col, len(ts_data) + 1)
         cells.Columns(cc_col + 1).Insert()
         et_col = cells.Columns(cc_col + 1)
         cells.Range(et_col.Cells(row, 1), et_col.Cells(cc_end_row, 1)).Value = et_formulas
         et_col.NumberFormat = "0.0"
         et_col.Cells(1, 1).Value = "Elapsed Time (sec)"
+        self.logger.debug("Elapsed Time column created.")
         return row, et_col, cc_end_row, cond_data
 
     def _add_raw_chart(self, cc_end_row, et_cc, first_data_row, ws):
+        self.logger.debug("Creating chart of conductivity vs. time")
         chart = xlcom.CreateChart(ws)
         xlcom.FormatChart(chart, None, "Time vs. Raw Voltage",
                           "Time (sec)", self.cv, False, False)
         x_rng, y_rng = xladdress.chart_range_strs(et_cc, et_cc + 1,
                                                   first_data_row, cc_end_row, ws.Name)
         xlcom.CreateDataSeries(chart, x_rng, y_rng)
+        return chart
 
-    def _add_cond_ave_80s(self, cc_end_row, cells, cond_data, et_cc):
+    def _calc_last_80_forms(self, cc_end_row, cond_data, et_cc):
+        self.logger.debug("Finding average of last 80 seconds.")
         ts_data = cond_data[0]
         end = len(ts_data) - 1
         last_pt = ts_data[-1]
@@ -166,14 +295,39 @@ class MixingTimeTest():
                 i += 1  # back up
                 break
         else:
-            raise MixingTimeAnalysisException("Failed to find last 80 sec of data")
+            raise MixingTimeAnalysisError("Failed to find last 80 sec of data")
         first_pv_cell = (i + 1, et_cc + 1)
         last_pv_cell = (cc_end_row, et_cc + 1)
         ave_last_80 = "=average(%s:%s)" % (xladdress.cellStr(*first_pv_cell),
                                            xladdress.cellStr(*last_pv_cell))
-        cells(1, et_cc + 4).Value = "Average last 80 sec"
-        cells(2, et_cc + 4).Value = ave_last_80
-        cells.Columns(et_cc + 4).AutoFit()
+        return ave_last_80
+
+    def _find_pv95(self, cells, cond_data, et_cc):
+        pc05 = cells(2, et_cc + 5).Value2
+        cf = cells(2, et_cc + 4).Value2
+        c95_min = cf - pc05
+        c95_max = cf + pc05
+        self.logger.debug("Searching for %s < x < %s", c95_min, c95_max)
+
+        # search for first data point not within 5% of final
+        # assume that data is sane, and flattens out over time
+        # and doesn't have weird spikes near the end
+        # and also isn't completely flat.
+
+        cond_pvs = cond_data[1]
+        last = len(cond_pvs) - 1
+        for i in range(last, 0, -1):
+            data = cond_pvs[i]
+            if not c95_min <= data <= c95_max:
+                i += 2  # +1 backup, +1 for 1-based row index
+                if i > last:
+                    i = last + 1
+                break
+        else:
+            # is this even possible?
+            raise MixingTimeAnalysisError("Failed to find point within 5% of final")
+        cells(i, et_cc + 2).Value = "**pv95"
+        return i
 
     def run_analysis(self):
         """
@@ -183,7 +337,7 @@ class MixingTimeTest():
 
         # Initialize
         self.logger.info("Beginning analysis on: %s", self.wb.Name)
-        ws = self.wb.Worksheets(1)
+        ws = self.ws
         cells = ws.Cells
 
         # 7.4.3 - Find first logger max log interval timestamp
@@ -194,11 +348,11 @@ class MixingTimeTest():
         et_cc = et_col.Column
 
         # 7.4.4 - Graph time vs conductivity pv
-        self._add_raw_chart(cc_end_row, et_cc, first_data_row, ws)
+        chart = self._add_raw_chart(cc_end_row, et_cc, first_data_row, ws)
 
         # 7.4.5 - Add initial conductivity, final ave conductivity over 80 sec
         free_col = cells.Columns(et_cc + 3)
-        for _ in range(4):
+        for _ in range(5):
             free_col.Insert()
 
         # 7.4.5.1 - Conductivity at t = 0 + header
@@ -208,7 +362,45 @@ class MixingTimeTest():
         cells.Columns(et_cc + 3).AutoFit()
 
         # 7.4.5.2 - final conductivity last 80 sec of batch
-        self._add_cond_ave_80s(cc_end_row, cells, cond_data, et_cc)
+        ave_last_80 = self._calc_last_80_forms(cc_end_row, cond_data, et_cc)
+        cells(1, et_cc + 4).Value = "Average last 80 sec"
+        cells(2, et_cc + 4).Value = ave_last_80
+        cells.Columns(et_cc + 4).AutoFit()
+
+        # 7.4.5.3 - calculate 5% of final - initial conductivity
+        cells(1, et_cc + 5).Value = "5% of (Final - Initial)"
+        cells(2, et_cc + 5).Value = "=0.05*(%s-%s)" % (xladdress.cellStr(2, et_cc + 4),
+                                                       xladdress.cellStr(2, et_cc + 3))
+        cells.Columns(et_cc + 5).AutoFit()
+
+        # 7.4.6 - highlight first measurement within 5% of final,
+        # provided that no subsequent points are not within 5% of final
+        row_pv95 = self._find_pv95(cells, cond_data, et_cc)
+
+        t95_cell = xladdress.cellStr(row_pv95, et_cc - 1)
+        r = 2
+        c = 1
+        lowest_addr = cells(first_data_row, et_cc - 1).Address
+        lowest = logger_timestamp
+        while True:
+            data = cells(r, c).Value
+            if not data:
+                break
+            if data < lowest:
+                lowest = data
+                lowest_addr = cells(r, c).Address
+            c += 3
+
+        # 7.4.7 - time from start of batch to t95
+        cells(1, et_cc + 6).Value = "T95"
+        cells(2, et_cc + 6).Value = "=(%s - %s)*24*60*60" % (t95_cell, lowest_addr)
+        cells(2, et_cc + 6).NumberFormat = "0.0"
+        self.t95 = cells(2, et_cc + 6).Value
+
+        # Format chart axes scale
+        ymin = math.floor(cells(2, et_cc + 3).Value2 * 0.9 * 10) / 10
+        ymax = math.ceil(cells(2, et_cc + 4).Value2 * 1.1 * 10) / 10
+        xlcom.FormatAxesScale(chart, None, None, ymin, ymax)
 
         return
 
@@ -232,10 +424,72 @@ def __purge_xl():
     xl.Quit()
 
 
-if __name__ == '__main__':
+def test_basic():
     __purge_xl()
-
     c = CompiledMixingTime()
     mt = c.add_csv("C:\\Users\\PBS Biotech\\Downloads 140827\\conductivity\\mt 1.2.csv")
     mt.cv = "phBRaw"
     c.analyze_all()
+
+
+def test_open_from_batchname():
+    __purge_xl()
+    c = CompiledMixingTime()
+    c.add_frombatchname("192.168.1.7", "mt 12rpm 1.1")
+    c.analyze_all()
+
+
+def M15_TR_005a():
+    def dl_all_batchnames(ipv4='192.168.1.7'):
+        """
+        download all batchnames from the given reactor and
+        analyze them.
+        """
+        app = HelloApp(ipv4)
+        app.login()
+        batches = app.getBatches()
+        names = batches.names_to_batches.keys()
+        dl_batchnames(ipv4, names)
+
+    def dl_batchnames(ipv4, batch_names):
+        c = CompiledMixingTime()
+        c.add_batchnames(ipv4, batch_names)
+        c.analyze_all()
+
+def M15_TR_005a_test():
+
+    path = "C:\\.replcache\\mixing_time_raw\\"
+
+    def dl_all_batchnames(ipv4='192.168.1.7'):
+        """
+        download all batchnames from the given reactor and
+        analyze them.
+        """
+        app = HelloApp(ipv4)
+        app.login()
+        batches = app.getBatches()
+        ids = batches.ids_to_batches.keys()
+
+        try:
+            os.makedirs(path)
+        except FileExistsError:
+            pass
+
+        for id in ids:
+            name = batches.getbatchname(id)
+            report = app.getdatareport_bybatchid(id)
+
+            with open(path + name + ".csv", 'wb') as f:
+                f.write(report)
+
+    def analyze_batches():
+        si = CompiledMixingSaveAsInfo(path.replace("_raw", ""))
+        c = CompiledMixingTime(saveas_info=si)
+        for file in os.listdir(path):
+            c.add_csv(path + file)
+        c.analyze_all()
+    analyze_batches()
+
+if __name__ == '__main__':
+    __purge_xl()
+    M15_TR_005a_test()
